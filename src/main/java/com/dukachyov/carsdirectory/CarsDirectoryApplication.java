70fd54809b03668e243b24393e0db17ad1463690
Ivan <dzullo@mail.ru>
2023-07-26 17:09:11 +0500

    Initial commit

---
+++ src/main/java/com/dukachyov/carsdirectory/CarsDirectoryApplication.java
@@ -0,0 +1,13 @@
+package com.dukachyov.carsdirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsDirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsDirectoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dukachyov/carsdirectory/CarsDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dukachyov.carsdirectory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsDirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
